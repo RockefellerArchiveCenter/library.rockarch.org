32cdc0a64f4d166fbbdc520c895987ca61174841
Hannah <hsistrunk@gmail.com>
2024-02-28 13:35:11 -0500

    improve create-index script

--- backstop_data/engine_scripts/create-index.js
+++ backstop_data/engine_scripts/create-index.js
@@ -1,25 +1,30 @@
 /**
- * Runs the create-index.js script before the Search Results scenario. Without this,
- * the search index is not available within the BackstopJS Docker container.
+ * Runs the create-index.js script before the Search Results scenario to automate
+ * rebuilding the index for each test.
  */
 
 const { exec } = require('child_process');
 
-function runScript(scriptPath, callback) {
-    // Execute the script
-    exec(`node ${scriptPath}`, (error, stdout, stderr) => {
-        if (callback) {
-            callback();
-        }
+function runScript(scriptPath) {
+    return new Promise((resolve, reject) => {
+        exec(`node ${scriptPath}`, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
     });
 }
 
 module.exports = async (page, scenario, vp, isReference) => {
-  await require('./puppet/loadCookies')(page, scenario);
+    await require('./puppet/loadCookies')(page, scenario);
 
-  // Run create-index.js from site build within Docker container
-  runScript('/src/_site/js/create-index.js', () => {
-      console.log('create-index script complete');
-  });
+    try {
+        // Run create-index.js from site build within Docker container
+        await runScript('./js/create-index.js');
+        console.log('New search index file created');
+    } catch (error) {
+        console.error('Error running script:', error);
+    }
 };
-
